81132a7beeb2f20186aaaa2877eed00c918a3b34
syelaseptania <48612589+syelaseptania@users.noreply.github.com>
2019-12-10 20:00:08 +0700

    Create koran.cpp

---
+++ koran.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <conio.h>
+#include "koran.h"
+#include "nama.h"
+using namespace std;
+int main ()
+{
+	int pilihan,baru,i;
+	tumpuk.atas=-1;
+	do
+	{
+		cout<<"\n=================================\n";
+		cout<<"Stock Koran (case sensitive)\n";
+		cout<<"=================================\n";
+		team();
+		cout<<"1.Input Kode Koran (angka)\n";
+		cout<<"2.Ambil Koran\n";
+		cout<<"3.Koran Tersedia\n";
+		cout<<"4.Lihat Tumpukan ke\n";
+		cout<<"5.Exit\n";
+		cout<<"\nPilihan = ";
+		cin>>pilihan;
+		switch(pilihan)
+		{
+    
+    
+    
+    }
+		cin.ignore();
+		cin.get();
+		system("cls");
+	}
+	while(pilihan!=5);
+
+	cin.get();
+	return 0;
+}
