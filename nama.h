72e5cba5e7f57091cc833210402fed0e4283a559
Annisa Nurwalikadani <48644318+annisanurwalikadani@users.noreply.github.com>
2019-12-10 19:49:26 +0700

    Add files via upload
    ini nama dan npm anggota

---
+++ nama.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+void team(){
+cout<<"Annisa Nurwalikadani - 1817051037\n";
+cout<<"Syela Septania - 1817051058\n";
+cout<<"Rika Ningtias Azhari - 1817051073\n";
+cout<<"=================================\n";
+}
